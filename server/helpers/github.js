d09d89b25b021713e91513572aa73743b1293de4
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-04-02 15:57:48 -0500

    Receiving repos from github api

--- server/helpers/github.js
+++ server/helpers/github.js
@@ -1,22 +1,33 @@
 const axios = require('axios');
 
 const config = require('../config');
-const testData = require('../data.json');
+const db = require('../database/index');
+// const testData = require('../data.json');
 
-const getReposByUsername = () => {
+const getReposByUsername = (username) => {
   // TODO: Use the axios module to get repos for a specific user from the github API
 
   // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   const options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/repos/${username}`,
     headers: {
       // https://developer.github.com/v3/#user-agent-required
       'User-Agent': 'request',
       'Authorization': `token ${config.GITHUB_TOKEN}`,
     },
   };
-
+  return new Promise((resolve, reject) =>{
+    axios.get(options.url, {
+      headers: options.headers
+    })
+    .then(repos => {
+      resolve(repos)
+    })
+    .catch(err => {
+      reject(err);
+    })
+  })
 };
 
 module.exports.getReposByUsername = getReposByUsername;
