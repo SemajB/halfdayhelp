ff88f35aaa5a24e179fc22bfab1e0b42a781313f
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-04-02 15:55:40 -0500

    added bodyparser

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const { Repos } = require('./api/repos');
 
 const CLIENT_PATH = path.resolve(__dirname, '../client/dist');
 const app = express();
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
 app.use(express.static(CLIENT_PATH));
 app.use('/api/repos', Repos);
 
