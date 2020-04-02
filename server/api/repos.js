d2dcb4fd48377d763bf4c8095940680069d81559
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-04-02 15:56:30 -0500

    working on POST request, getting errors

--- server/api/repos.js
+++ server/api/repos.js
@@ -1,9 +1,21 @@
 const { Router } = require('express');
 const Repos = Router();
+const db = require('../database/index');
+const gitHelper = require('../helpers/github');
 
 Repos.get('/', (req, res) => {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  res.status(200);
+  db.getTop25Repos()
+  .then(repos => {
+    res.status(200);
+    res.send(repos);
+  })
+  .catch(err => {
+    console.error(err);
+    res.sendStatus(500);
+  })
 });
 
 Repos.post('/', (req, res) => {
@@ -11,6 +23,20 @@ Repos.post('/', (req, res) => {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  let username = req.body.username;
+  gitHelper.getReposByUsername(username)
+  .then(repos => {
+    repos.data.forEach(repo => {
+      db.saveRepo(repo)
+      .then(savedRepo => {
+        res.statusCode(201);
+      })
+      .catch(err => {
+        console.error(err);
+        res.sendStatus(500);
+      })
+    })
+  })
 });
 
 module.exports = {
