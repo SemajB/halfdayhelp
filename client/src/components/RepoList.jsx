9621245424e6483348c41f1f3810d0d96ed11ec3
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-04-09 15:34:30 -0500

    Loading top 25 repos on the page

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -5,6 +5,12 @@ const RepoList = props => (
   <div>
     <h4>Repo List Component</h4>
     Top {props.repos.length} repos by watchers.
+    <ul>
+      {props.repos.map(repo => {
+        console.log(repo);
+       return (<li key={repo._id}>{repo.login}: <a href={repo.html_url}>{repo.name}</a></li>)
+      })}
+    </ul>
   </div>
 );
 
